34a013e6ffe9292a621d146aba9271216931d13d
drmitia <drdrmitia@gmail.com>
2024-04-14 21:25:02 +0300

    6homework

---
+++ EpamHW6.1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace EpamHW6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int am = Convert.ToInt32(Console.ReadLine());
+            int bm = Convert.ToInt32(Console.ReadLine());
+            int cm = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(gcdMain(am, bm, cm)); 
+            
+            static int gcdMain(int a, int b, int c)
+            {
+                static int gcd(int a, int b)
+                {
+                    while (a != b)
+                    {
+                        if (a > b)
+                        {
+                            a = a - b;
+                        }
+                        else
+                        {
+                            b = b - a;
+                        }
+                    }
+                    return a;
+                }
+                return gcd(gcd(a, b), c);
+            }
+
+
+        }
+        
+        
+    }
+}
---
+++ EpamHW6.2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace EpamHW6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 12;
+            int nearestPrime = FindNearestPrime(b);
+            Console.WriteLine(nearestPrime);
+            static bool IsPrime(int n)
+            {
+                if (n <= 1)
+                {
+                    return false;
+                }
+
+                for (int i = 2; i * i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            static int FindNearestPrime(int num)
+            {
+                int smallerPrime = num - 1;
+                int largerPrime = num + 1;
+
+                while (true)
+                {
+                    if (IsPrime(largerPrime))
+                    {
+                        return largerPrime;
+                    }
+                    else if (IsPrime(smallerPrime))
+                    {
+                        return smallerPrime;
+                    }                   
+                    smallerPrime--;
+                    largerPrime++;
+                }
+            }
+
+        }
+    }
+}
